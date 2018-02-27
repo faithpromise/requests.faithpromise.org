f064581a92f0d25f5deb37fb7cf1a7ad245ec1ef
Brad Roberts <brad@bradroberts.net>
2018-02-26 19:02:10 -0500

    Updated text

--- resources/assets/js/route_components/route-worship-tech.vue
+++ resources/assets/js/route_components/route-worship-tech.vue
@@ -3,7 +3,7 @@
   <ticket-form
           type="worship-tech"
           title="Tech Maintenance"
-          intro="Use this form if you have worship tech that needs repair."
+          intro="Use this form if you have production equipment that needs repair."
           message-label="Please help us by describing the issue with as much detail as possible.">
   </ticket-form>
 
--- resources/assets/js/tickets/ticket-types.js
+++ resources/assets/js/tickets/ticket-types.js
@@ -5,5 +5,5 @@ export default [
     { slug: 'facility', name: 'Facility', description: 'From repairs and replacements to construction. We\'ve got you covered.', },
     { slug: 'finance', name: 'Human Resources', description: 'Anything PO\'s, CC\'s, bills, invoices, budgets, or expenses.', },
     { slug: 'hr', name: 'Human Resources', description: 'Payroll, benefits, job descriptions, or anything HR.', },
-    { slug: 'worship-tech', name: 'Worship Tech', description: 'Mics, projectors, lights, or other worship equipment issues.', },
+    { slug: 'worship-tech', name: 'Production Maintenance', description: 'Mics, projectors, lights, or other production equipment issues.', },
 ]
