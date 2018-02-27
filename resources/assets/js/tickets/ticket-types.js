422e933057ce9af9cb99b1825eb17f9329c34b3b
Brad Roberts <brad@bradroberts.net>
2018-02-27 09:20:43 -0500

    Updated text

--- resources/assets/js/tickets/ticket-types.js
+++ resources/assets/js/tickets/ticket-types.js
@@ -3,7 +3,7 @@ export default [
     { slug: 'creative', name: 'Creative Team', description: 'Promotional materials & videos.', },
     { slug: 'web', name: 'Website Update', description: 'Additions and changes to any of our websites.', },
     { slug: 'facility', name: 'Facility', description: 'From repairs and replacements to construction. We\'ve got you covered.', },
-    { slug: 'finance', name: 'Human Resources', description: 'Anything PO\'s, CC\'s, bills, invoices, budgets, or expenses.', },
+    { slug: 'finance', name: 'Finance', description: 'Anything PO\'s, CC\'s, bills, invoices, budgets, or expenses.', },
     { slug: 'hr', name: 'Human Resources', description: 'Payroll, benefits, job descriptions, or anything HR.', },
     { slug: 'worship-tech', name: 'Production Maintenance', description: 'Mics, projectors, lights, or other production equipment issues.', },
 ]
